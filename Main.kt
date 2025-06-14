a1c2e191ec9c5f5939b1f38fb1e86f2c33a92dc1
Aline Soares <aline01600@gmail.com>
2025-06-14 20:41:17 -0300

    Adicionar função  ordenarCrescente

--- Main.kt
+++ Main.kt
@@ -1,4 +1,9 @@
 fun main(){
     val numeros = listOf(5,3,8,3,2,8,10,1)
     println("Lista original: $numeros")
+
+}
+
+fun ordenarCrescente(Lista: List<Int>): List<Int>{
+    return lista.sorted()
 }
