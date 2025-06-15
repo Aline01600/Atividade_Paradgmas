1303ec8e05660348027a09a8719fb6aefbc41adb
Aline Soares <aline01600@gmail.com>
2025-06-14 23:51:25 -0300

    chamar função ordenarCrescente no main

--- Main.kt
+++ Main.kt
@@ -1,6 +1,7 @@
 fun main(){
     val numeros = listOf(5,3,8,3,2,8,10,1)
     println("Lista original: $numeros")
+    println("Ordenação crescente: " + executar_estrategia(numeros, ::ordenarCrescente))
 }
 fun executar_estrategia(lista: List<Int>, estrategia: (List<Int>) -> List<Int>): List<Int> {
     return estrategia(lista)
