2a5d9166faeec1d0f0be62c44e4c77fdec23606d
Aline Soares <aline01600@gmail.com>
2025-06-14 21:08:09 -0300

    Adicionar função executar_estrategia

--- Main.kt
+++ Main.kt
@@ -1,9 +1,14 @@
 fun main(){
     val numeros = listOf(5,3,8,3,2,8,10,1)
     println("Lista original: $numeros")
+    println("Ordenação crescente: " + executar_estrategia(numeros, ::ordenarCrescente))
 
 }
 
-fun ordenarCrescente(Lista: List<Int>): List<Int>{
+fun executar_estrategia(lista: List<Int>, estrategia: (List<Int>) -> List<Int>): List<Int> {
+    return estrategia(lista)
+}
+
+fun ordenarCrescente(lista: List<Int>): List<Int>{
     return lista.sorted()
 }
