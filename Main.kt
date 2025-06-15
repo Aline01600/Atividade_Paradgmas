17d694b8e3cf9ec01e8d14e563d0db17e4f7856f
Aline Soares <aline01600@gmail.com>
2025-06-14 23:27:06 -0300

    Adicionar função executar_estrategia

--- Main.kt
+++ Main.kt
@@ -2,3 +2,7 @@ fun main(){
     val numeros = listOf(5,3,8,3,2,8,10,1)
     println("Lista original: $numeros")
 }
+
+fun executar_estrategia(lista: List<Int>, estrategia: (List<Int>) -> List<Int>): List<Int> {
+    return estrategia(lista)
+}
